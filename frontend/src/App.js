a21defb2c6ed097f410b45a9a7d0e707256fecbf
Niraj Mantri <112097023+nirajmantri56@users.noreply.github.com>
2023-12-03 12:57:16 +0530

    changes in FRONTEND

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Projectpage from "./pages/Projectpage";
 import Home from './pages/Home';
 
 import { Toaster } from 'react-hot-toast';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import SignupPage from './pages/SignupPage';
 import Signinpage from './pages/Signinpage';
 import ProjectDetails from './pages/ProjectDetails';
